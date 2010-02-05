327cbddbb9d4872e7f839e0df4a507c0796ae61c
Stephen <twocrazymen23@gmail.com>
2010-02-06 05:13:13 +1300

    Updating for plans to work

--- smarty/ecom/libs/controllers/PlanController.php
+++ smarty/ecom/libs/controllers/PlanController.php
@@ -1,14 +1,28 @@
 <?php
-class HomeController extends MainController {
+class PlanController extends MainController {
         var $mysqli;
+	var $plan; // TODO: Make this an object instead, because we all know objects are winner
 
 	function __construct() {
 		parent::__construct();
-		$this->Smarty->assign('title', 'Home');
+		$this->GetPlan($_GET['plan']);
+		print_r($this->plan);
+		$this->Smarty->assign('title', 'Plans - '.$this->plan['name']);
 	}
 	function Output() {
 		$this->GetPlans();
-		$this->Smarty->display('Home.tpl');
+		$this->Smarty->display('Plan.tpl');
+	}
+	function GetPlan($UUID) {
+		if (!($this->mysqli)) {
+			$this->mysqli = mysqli_init2();
+		}
+		// zomg too long, but to create an alias to mysql_real_escape_string, globalize mysqli and do it that way, or to do a mysqli::prepare... Decisions decisions
+		$UUID = $this->mysqli->real_escape_string($UUID);
+		
+		$result = $this->mysqli->query("select * from `plans` where `plans`.`uuid` LIKE '".$UUID."'");
+		$this->plan = $result->fetch_assoc();
+		$result->close();
 	}
 }
 ?>
--- smarty/ecom/templates_c/%%41^411^411D7689%%Footer.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2009-12-23 03:10:42
-         compiled from Footer.tpl */ ?>
-</html>
